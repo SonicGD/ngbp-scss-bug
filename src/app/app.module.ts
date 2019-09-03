6aefe350d6a9735630f13b39c548967da5a4d626
George <sonicgd@gmail.com>
2019-09-03 12:21:05 +0500

    commit example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import 'zone.js/dist/zone';
 import { AppComponent } from './app.component';
 
 @NgModule({
